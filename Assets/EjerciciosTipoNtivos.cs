f461233b34078cc6c8fc51f1971b3768839a89cd
wurst-bruno <101563441+wurst-bruno@users.noreply.github.com>
2022-06-10 12:10:19 -0300

    clase10-6

---
+++ Assets/EjerciciosTipoNtivos.cs
@@ -0,0 +1,63 @@
+﻿
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjerciciosTipoNtivos : MonoBehaviour
+{
+    public float mifloat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HolaMundoEnLaConsola();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            EscribirParametroEnConsola("El martes se entregan los juegos");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            EscribirParametroEnConsola(ObtenerParteEnteraEnString(mifloat));
+        }
+    }
+       ////Definir una función que escriba en consola “Hola Mundo!” al ser invocada.Invocar la función al presionar la barra espaciadora.
+
+    void HolaMundoEnLaConsola()
+    {
+        Debug.Log("Hola Mundo!");
+
+    }
+
+    //Definir una función que escriba en consola la cadena pasada por parámetro.
+
+    void EscribirParametroEnConsola(string cadena)
+    {
+        Debug.Log(cadena);
+    }
+
+    //Recibe un float por parámetro y lo procesa para devolver la parte entera 
+    //como un string. mostrar el dato devuelto en la consola utilizando la funcion del punto 1.2
+
+    string ObtenerParteEnteraEnString(float num)
+    {
+        string retorno = "";
+        retorno =  Mathf.Floor(num).ToString();
+        return retorno;
+
+    }
+
+    //Definir una función que reciba tres enteros y devuelva el mayor de ellos.Si son los tres iguales 
+    //retornar -1.  Mostrar el dato devuelto en la consola utilizando la función del punto 1.2.
+
+
+}
---
+++ Assets/FuncionParaBoton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuncionParaBoton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ComportamientoBoton();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ComportamientoBoton()
+    {
+        Debug.Log("hi");
+    }
+}
